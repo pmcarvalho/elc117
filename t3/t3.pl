d775094ee380a22e1687c56df7239cd6ad0c101c
pmcarvalho <pmcarvalho@inf.ufsm.br>
2018-04-15 13:43:05 -0300

    ADD ex2, ex3

--- t3/t3.pl
+++ t3/t3.pl
@@ -4,3 +4,13 @@ zeroInit(L) :-
     L = [H|_],
     H =:= 0.
 
+%2. Defina um predicado has5(L) que é verdadeiro se L for uma lista de 5 elementos.
+% Resolva este exercício sem usar um predicado auxiliar.
+
+has5(L) :-
+    L = [_,_,_,_,_].
+
+%3. Defina um predicado hasN(L,N) que é verdadeiro se L for uma lista de N elementos.
+
+hasN(L,N):-
+    length(L,N).
